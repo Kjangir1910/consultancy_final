a94f79d4284e6bace090e521adbe2c6ab765298b
Kjangir1910 <129332297+Kjangir1910@users.noreply.github.com>
2025-06-08 02:15:29 +0530

    Update Services.jsx

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,5 +1,34 @@
 
 
+// import React from "react";
+// import services from "../components/serviceData";
+// import ServiceCard from "../components/ServiceCard";
+
+// function ServicesPage() {
+//   return (
+//     <div className="container mx-auto px-4 py-10">
+//       <h2 style={{margin: '30px 0px 30px 0px'}} className="text-4xl font-bold text-center mb-10 text-blue-900">
+//         Explore Our Services
+//       </h2>
+//       <div style={{marginBottom : '60px'}} className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+
+//         {services.map((service, index) => (
+//           <ServiceCard
+//             key={index}
+//             title={service.title}
+//             description={service.description}
+//             subServices={service.subServices}
+//             image={service.image}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ServicesPage;
+
+
 import React from "react";
 import services from "../components/serviceData";
 import ServiceCard from "../components/ServiceCard";
@@ -7,11 +36,17 @@ import ServiceCard from "../components/ServiceCard";
 function ServicesPage() {
   return (
     <div className="container mx-auto px-4 py-10">
-      <h2 style={{margin: '30px 0px 30px 0px'}} className="text-4xl font-bold text-center mb-10 text-blue-900">
+      <h2
+        className="text-4xl font-bold text-center mb-10 text-blue-900"
+        style={{ margin: "30px 0px 30px 0px" }}
+      >
         Explore Our Services
       </h2>
-      <div style={{marginBottom : '60px'}} className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
 
+      <div
+        className="grid gap-6 sm:grid-cols-1 md:grid-cols-2"
+        style={{ marginBottom: "60px" }}
+      >
         {services.map((service, index) => (
           <ServiceCard
             key={index}
